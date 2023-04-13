9f5aafcdef51eee2eed33fcea24d7dd3761936a3
Martin Westgate <martinjwestgate@gmail.com>
2023-04-13 14:01:05 +1000

    add function to build cards form qmd yaml

--- R/card_functions.R
+++ R/card_functions.R
@@ -1,4 +1,26 @@
+#' Read yaml data and use to create card
+#' 
+#' This is in early dev
+#' @param file path to an 'article' qmd file
+#' @importFrom yaml read_yaml
+#' @export
+#' 
+create_card_from_qmd <- function(file){
+  x <- read_yaml(file) # makes list from yaml for a given article
+  create_card(
+    color1 = x$color_1,
+    headline = x$title,
+    img_src = x$image_url,
+    alt = x$alt_text
+  )
+}
 
+
+#' Create html card using R
+#' 
+#' This is the underlying function, written by Dax
+#' @importFrom htmltools tags
+#' @export
 create_card <- function(person_page, img_src, headline, alt, name, color1) {
   tags$div(
     class = "card-column",
@@ -9,5 +31,4 @@ create_card <- function(person_page, img_src, headline, alt, name, color1) {
       p(class = "headline", headline),
       tags$img(class ="card-image", src = img_src, alt = name),
       p(class = "headline-2", headline)))
-      
 }
