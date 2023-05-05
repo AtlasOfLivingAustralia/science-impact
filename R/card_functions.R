1339dd436e87edddec2d927c44382cb171334e03
Dax Kellie <dax.kellie@csiro.au>
2023-05-05 14:02:49 +1000

    Add unfinished tests of javascript carousel

--- R/card_functions.R
+++ R/card_functions.R
@@ -21,14 +21,15 @@ create_card_from_qmd <- function(file){
 #' This is the underlying function, written by Dax
 #' @importFrom htmltools tags
 #' @export
-create_card <- function(person_page, img_src, headline, alt, name, color1) {
-  tags$div(
-    class = "card-column",
-    tags$button(
-      class = "paper-card", 
-      href = person_page, 
-      style = paste0("background-color:", color1),
-      p(class = "headline", headline),
-      tags$img(class ="card-image", src = img_src, alt = name),
-      p(class = "headline-2", headline)))
+create_card <- function() {
+  # tags$div(
+  #   class = "card-column",
+  #   tags$button(
+  #     class = "paper-card", 
+  #     href = person_page, 
+  #     style = paste0("background-color:", color1),
+  #     p(class = "headline", headline),
+  #     tags$img(class ="card-image", src = img_src, alt = name),
+  #     p(class = "headline-2", headline)))
+  tags$div(class = "paper-card")
 }
